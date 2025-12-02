3b74880c2298fcf2f4674f559a5b7067c21ef497
Henrik Mattsson <henkehenke@gmail.com>
2025-12-02 15:26:28 +0100

    Modify CSS

--- backend/src/main/java/hm222yj_2dv515/a1/backend/BackendApplication.java
+++ backend/src/main/java/hm222yj_2dv515/a1/backend/BackendApplication.java
@@ -19,15 +19,6 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-		// DataService dataService = new DataService(); // Pass argumeter small or large // No need anymore, change in DataService.java instead.
-		// dataService.deleteData();
-		// dataService.loadData();
-		// // ArrayList<DataReader> results = new ArrayList<>(dataController.getData());
-		// // System.out.println(results);
-		// UserAndMovieService userAndMovieService = new UserAndMovieService(dataService);
-		// EuclideanService euclideanService = new EuclideanService(userAndMovieService);
-		// List<RecommendedMovie> recommendations = euclideanService.topRecommendations(4, 3);
-		// System.out.println(recommendations);
 	}
 
 
